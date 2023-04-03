fad97d24c606a66fa76b0834d880234b37ab8e2e
Dng16 <dongyang0016@gmail.com>
2023-04-03 15:20:04 +0800

    final commit

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const form1 = document.querySelector('.form-1');
+const form2 = document.querySelector('.form-2');
+const email1 = document.querySelector('.email-1');
+const email2 = document.querySelector('.email-2');
+
+
+form1.addEventListener('submit',e =>{
+  e.preventDefault();
+    const emailVal1 = email1.value;
+
+    if(!isEmail(emailVal1)){
+    
+      form1.classList.add('error');
+    }
+    else{
+      form1.classList.remove('error');
+    }
+    
+  }
+
+);
+form2.addEventListener('submit',e =>{
+  e.preventDefault();
+    const emailVal2 = email2.value;
+ 
+    if(!isEmail(emailVal2)){
+    
+      form2.classList.add('error');
+    }
+    else{
+      form2.classList.remove('error');
+    }
+    
+  }
+
+);
+
+function isEmail(email) { 
+  return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(email);
+} 
